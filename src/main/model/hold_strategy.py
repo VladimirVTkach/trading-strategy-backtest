b92ed41bc880fcf1d44449972821b70957b8b80c
Volodymyr <vladimirv.tkach@gmail.com>
2021-06-15 22:19:53 +0300

    HoldStrategy class has been implemented

---
+++ src/main/model/hold_strategy.py
@@ -0,0 +1,15 @@
+from strategy import Strategy
+
+
+class HoldStrategy(Strategy):
+    def run(self, candlesticks, initial_balance):
+        buy_price = candlesticks[0].open
+        asset_amount = initial_balance / buy_price
+
+        balance_history = [initial_balance]
+        for candlestick in candlesticks[1:]:
+            current_price = candlestick.close
+            current_balance = asset_amount * current_price
+            balance_history.append(current_balance)
+
+        return balance_history
