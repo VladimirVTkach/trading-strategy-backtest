f06b4a4c1cbf495843086f40018e5ec8b91433c1
Volodymyr <vladimirv.tkach@gmail.com>
2021-06-15 22:22:28 +0300

    StrategyFactory class has been created

---
+++ src/main/model/strategy_factory.py
@@ -0,0 +1,9 @@
+from hold_strategy import HoldStrategy
+
+
+class StrategyFactory:
+    def __init__(self):
+        self._strategies = {"hold": HoldStrategy()}
+
+    def get_strategy(self, name):
+        return self._strategies[name]
