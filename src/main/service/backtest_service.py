3c8bc9ba0b213bc254c3329b0cd1d4674fc3d56e
Volodymyr <vladimirv.tkach@gmail.com>
2021-06-16 09:27:29 +0300

    BacktestService class has been created

---
+++ src/main/service/backtest_service.py
@@ -0,0 +1,34 @@
+from model.market_type import MarketType
+from model.time_interval import TimeInterval
+
+import matplotlib.pyplot as plt
+
+
+class BacktestService:
+    def __init__(self, market_data_service, strategy_factory):
+        self._market_data_service = market_data_service
+        self._strategy_factory = strategy_factory
+        self._current_file_index = 1
+
+    async def get_backtest_result(self,
+                                  symbol: str,
+                                  start_date: int,
+                                  initial_balance: int,
+                                  interval: TimeInterval,
+                                  strategy: str):
+        candlesticks = await self._market_data_service.get_historical_candlesticks(symbol=symbol,
+                                                                                   market_type=MarketType.SPOT,
+                                                                                   interval=interval,
+                                                                                   start_date=start_date)
+        strategy = self._strategy_factory.get_strategy(strategy)
+        balance_history = strategy.run(candlesticks=candlesticks,
+                                       initial_balance=initial_balance)
+
+        plt.plot(balance_history)
+
+        filename = f'balance_history_{self._current_file_index}.png'
+        plt.savefig(filename)
+
+        self._current_file_index += 1
+
+        return filename
