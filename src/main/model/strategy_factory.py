ad2742513f96e34d2bcc82ccec96d5e6efe2fc68
Volodymyr <vladimirv.tkach@gmail.com>
2021-06-16 21:37:27 +0300

    MeanPriceBasedStrategy instance has been added

--- src/main/model/strategy_factory.py
+++ src/main/model/strategy_factory.py
@@ -1,9 +1,11 @@
 from model.hold_strategy import HoldStrategy
+from model.mean_price_based_strategy import MeanPriceBasedStrategy
 
 
 class StrategyFactory:
     def __init__(self):
-        self._strategies = {"hold": HoldStrategy()}
+        self._strategies = {"hold": HoldStrategy(),
+                            "mean_price_based": MeanPriceBasedStrategy()}
 
     def get_strategy(self, name):
         return self._strategies[name]
